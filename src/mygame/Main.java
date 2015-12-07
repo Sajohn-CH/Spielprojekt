4904b46dbd6cd87430ce3b61b6c97531ba266b94
Sajohn-CH <samuel.martin@macrolino.ch>
2015-12-07 09:40:29 +0100

    Added first 3D

---
+++ src/mygame/Main.java
@@ -0,0 +1,181 @@
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import de.lessvoid.nifty.Nifty;
+
+/**
+ * test
+ * @author normenhansen
+ */
+
+public class Main extends SimpleApplication implements ActionListener{
+    private Spatial scene;
+    protected static Main app;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl sceneC;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false;
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
+    private Node n;
+    
+    public static void main(String[] args) {
+        System.out.println("test");
+        app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+       scene = assetManager.loadModel("Scenes/newScene.j3o");
+        scene.setLocalScale(2f);
+        
+        setUpKeys();
+        //scene.setLocalTranslation(0, -5, 0);
+        //flyCam.setMoveSpeed(30);
+        n = new Node();             // attach to n to let disappear when player is there
+        Node n1 = new Node();       // attach to n1 to make collision resistant
+                
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.White);
+        geom.setMaterial(mat);
+        geom.setLocalTranslation(5, 2, 5);
+        n.attachChild(geom);
+        
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape((Node) scene);
+        sceneC = new RigidBodyControl(sceneShape, 0);
+        scene.addControl(sceneC);
+        
+        Box c = new Box(1, 1, 1);
+        Geometry geom1 = new Geometry("Box1", c);
+        Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat1.setColor("Color", ColorRGBA.Blue);
+        geom1.setMaterial(mat1);
+        geom1.setLocalTranslation(10, 2, 10);
+        n1.attachChild(geom1);
+        
+        Box d = new Box (5,1,5);
+        Geometry large = new Geometry("largeBox", d);
+        large.setMaterial(mat1);
+        large.setLocalTranslation(20, 2, 20);
+        n1.attachChild(large);
+        
+        Sphere e = new Sphere(100, 100, 2);
+        Geometry sphere = new Geometry("sphere", e);
+        sphere.setMaterial(mat1);
+        sphere.setLocalTranslation(-20, 2, -20);
+        n1.attachChild(sphere);
+        
+        CollisionShape boxShape = CollisionShapeFactory.createMeshShape((Node) n1);
+        RigidBodyControl boxC = new RigidBodyControl(boxShape, 0);
+        n1.addControl(boxC);
+              
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+        player.setJumpSpeed(20);
+        player.setFallSpeed(90);
+        player.setGravity(30);
+        player.setPhysicsLocation(new Vector3f(0, 10, 0));
+                
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+    
+        rootNode.attachChild(n);
+        rootNode.attachChild(n1);
+        rootNode.attachChild(scene);
+        bulletAppState.getPhysicsSpace().add(sceneC);
+        bulletAppState.getPhysicsSpace().add(boxC);
+        bulletAppState.getPhysicsSpace().add(player);
+        
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay( assetManager, inputManager, audioRenderer, guiViewPort);
+        //Create a new NiftyGui objects
+        Nifty nifty = niftyDisplay.getNifty();
+        //Read yout XML and initialize your custom Screen Controller
+        nifty.fromXml("Interface/screen.xml", "start");
+        //attach the Niftry display to the gui view port as a processor
+        guiViewPort.addProcessor(niftyDisplay);
+        //disable the fly cam
+        flyCam.setDragToRotate(true);
+        
+    }
+    
+    private void setUpKeys() {
+    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+    inputManager.addListener(this, "Left");
+    inputManager.addListener(this, "Right");
+    inputManager.addListener(this, "Up");
+    inputManager.addListener(this, "Down");
+    inputManager.addListener(this, "Jump");
+  }
+    
+    public void onAction(String binding, boolean isPressed, float tpf) {
+    if (binding.equals("Left")) {
+      left = isPressed;
+    } else if (binding.equals("Right")) {
+      right= isPressed;
+    } else if (binding.equals("Up")) {
+      up = isPressed;
+    } else if (binding.equals("Down")) {
+      down = isPressed;
+    } else if (binding.equals("Jump")) {
+      if (isPressed) { player.jump(); }
+    }
+  }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        for (int i = 0; i < n.getChildren().size(); i++)
+            if(!n.getChildren().isEmpty())
+                if((cam.getLocation().getX()-1 < n.getChild(i).getLocalTranslation().add(n.getLocalTranslation()).getX() && cam.getLocation().getX()+1 > n.getChild(i).getLocalTranslation().add(n.getLocalTranslation()).getX()) && (cam.getLocation().getZ()-1 < n.getChild(i).getLocalTranslation().add(n.getLocalTranslation()).getZ() && cam.getLocation().getZ()+1 > n.getChild(i).getLocalTranslation().add(n.getLocalTranslation()).getZ()))
+                    n.detachChildAt(0); // If cam is in box -> detach box
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+    
+}
---
+++ src/mygame/MyStartScreen.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.screen.KeyInputHandler;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+/**
+ *
+ * @author samuel
+ */
+public class MyStartScreen extends AbstractAppState implements ScreenController, KeyInputHandler{
+    private Nifty nifty;
+    private Application app;
+    
+    
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = app;
+        //TODO: initialize your AppState, e.g. attach spatials to rootNode
+        //this is called on the OpenGL thread after the AppState has been attached
+    }
+    
+    @Override
+    public void update(float tpf) {
+        //TODO: implement behavior during runtime
+    }
+    
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        //TODO: clean up what you initialized in the initialize method,
+        //e.g. remove all spatials from rootNode
+        //this is called on the OpenGL thread after the AppState has been detached
+    }
+
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void onStartScreen() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void onEndScreen() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /** custom methods */
+    public void startGame(String nextScreen) {
+        nifty.gotoScreen(nextScreen);
+        Main.app.getFlyByCamera().setDragToRotate(false);
+        
+    }
+    
+    public void quitGame() {
+       app.stop();
+    }
+    
+    public String getPlayerName() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    //From KeyInputHandler
+    public boolean keyEvent(NiftyInputEvent inputEvent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
